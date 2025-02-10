37ac5335854421cbda185144f0d3ae56c78fb935
Will Stuchbury <w.stuchbury@pathtrack.co.uk>
2025-02-10 10:46:48 +0000

    Added VOC scaling to EH solar parser

--- parsers/parseEHSolar.py
+++ parsers/parseEHSolar.py
@@ -7,10 +7,8 @@ def parseEHSolarLine(data, commonHeader, lineNum, outputArr):
 
     interval = data[5] + (data[6]<<8)
 
-    vocArr = data[7::3]
-    harvLArr = data[8::3]
-    harvHArr = data[9::3]
-    harvArr = harvLArr + (harvHArr << 8)
+    vocArr = (data[7::3]) * 0.01
+    harvArr = data[8::3] + (data[9::3] << 8)
 
     solarValues = [{"line":lineNum,"time":0,"VOC":voc, "harvCount":harv} for voc, harv in zip(vocArr, harvArr)]
 
