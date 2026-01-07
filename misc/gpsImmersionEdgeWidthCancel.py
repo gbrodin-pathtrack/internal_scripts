3ada24b3b821f33f5e6f11ce45a91f02e8f1c4ce
Will Stuchbury <w.stuchbury@pathtrack.co.uk>
2026-01-07 09:39:16 +0000

    Combined legring analysis scripts for edge width and gps cancels

---
+++ misc/gpsImmersionEdgeWidthCancel.py
@@ -0,0 +1,95 @@
+import pandas as pd
+import numpy as np
+from os import listdir
+from os.path import isfile, join
+
+USE_PICKLE = True
+
+SUBSAMPLE = 8
+
+if USE_PICKLE:
+    wantedExtension = ".pkl"
+else:
+    wantedExtension = ".csv"
+
+gpsWantedFiles = [f for f in listdir("./") if isfile(join("./", f)) and f.endswith("_GPS_Obs"+wantedExtension)]
+immWantedFiles = [f for f in listdir("./") if isfile(join("./", f)) and f.endswith("_Immersion"+wantedExtension)]
+
+gpsFileName = gpsWantedFiles[0]
+immFileName = immWantedFiles[0]
+
+#read files into data frames
+if USE_PICKLE:
+    obsDF = pd.read_pickle(gpsFileName)
+    immDF = pd.read_pickle(immFileName)
+else:
+    obsDF = pd.read_csv(gpsFileName)
+    immDF = pd.read_csv(immFileName)
+
+obsDF["cancelledZero"] = obsDF["numSV"] == 0
+obsDF["cancelledWet"] = (obsDF["numSV"] > 0) & (obsDF["numSV"] < 5) & (obsDF["TTF"] < 20)
+
+obsDF["cancelled"] = obsDF["cancelledZero"] | obsDF["cancelledWet"]
+obsDF["saved"] = 20 - obsDF["TTF"]
+
+tagImmDFs : dict[int, pd.DataFrame] = {}
+
+for tagID in immDF["tagID"].unique():
+    tagImmDFs[tagID] = immDF.loc[immDF["tagID"] == tagID].copy()
+
+prevWet = 0
+
+def genPrevWet(row):
+    global prevWet
+    ret = prevWet
+    if row.immersed == True:
+        prevWet += 1
+    else:
+        prevWet = 0
+    return ret
+
+numEdges = 0
+numSingleEdges = 0
+
+tagEdgeDFs : dict[int, pd.DataFrame] = {}
+
+for tagImmDF in tagImmDFs.values():
+    tagImmDF.drop(tagImmDF.loc[(tagImmDF.index % SUBSAMPLE) != 0].index, inplace=True)
+    tagImmDF.reset_index(drop=True, inplace=True)
+    prevWet = 0
+    tagImmDF["prevWet"] = tagImmDF.apply(genPrevWet, axis=1)
+    edgeDF = tagImmDF.loc[(tagImmDF["immersed"] == False) & (tagImmDF["prevWet"] > 0)].copy()
+    numEdges += len(edgeDF)
+    numSingleEdges += len(edgeDF.loc[edgeDF["prevWet"] == 1])
+    tagEdgeDFs[edgeDF.tagID.iloc[0]] = edgeDF.reset_index(drop=True)
+
+print("Percentage of single edges: %d%%" % ((numSingleEdges / numEdges) * 100))
+
+print()
+
+def getPrevWet(obs):
+    global tagEdgeDFs
+    tagEdgeDF = tagEdgeDFs[obs.tagID]
+    tagEdgeDF["timeDiff"] = (tagEdgeDF["datetime"] - obs.datetime).dt.total_seconds()
+    triggerIdx = tagEdgeDF.loc[tagEdgeDF["timeDiff"] < 0, "timeDiff"].idxmax()
+    return tagEdgeDF.prevWet.iloc[triggerIdx]
+
+obsDF["prevWet"] = obsDF.apply(getPrevWet, axis=1)
+
+singleDF = obsDF.loc[obsDF["prevWet"] == 1].copy()
+multiDF = obsDF.loc[obsDF["prevWet"] > 1].copy()
+
+print("Triggered by single wet:")
+print("Percent cancelled zero: %d%%" % ((len(singleDF.loc[singleDF["cancelledZero"] == True]) / len(singleDF)) * 100))
+print("Percent cancelled wet: %d%%" % ((len(singleDF.loc[singleDF["cancelledWet"] == True]) / len(singleDF)) * 100))
+print("Average cancel saving: %.2fs" % singleDF.loc[singleDF["cancelled"] == True, "saved"].mean())
+print("Success rate of non cancelled: %d%%" % ((len(singleDF.loc[(singleDF["numSV"] >= 5) & (singleDF["cancelled"] == False)]) / len(singleDF.loc[singleDF["cancelled"] == False])) * 100))
+print("Overall success rate: %d%%" % ((len(singleDF.loc[singleDF["numSV"] >= 5]) / len(singleDF)) * 100))
+
+print()
+print("Triggered by multiple wet:")
+print("Percent cancelled zero: %d%%" % ((len(multiDF.loc[multiDF["cancelledZero"] == True]) / len(multiDF)) * 100))
+print("Percent cancelled wet: %d%%" % ((len(multiDF.loc[multiDF["cancelledWet"] == True]) / len(multiDF)) * 100))
+print("Average cancel saving: %.2fs" % multiDF.loc[multiDF["cancelled"] == True, "saved"].mean())
+print("Success rate of non cancelled: %d%%" % ((len(multiDF.loc[(multiDF["numSV"] >= 5) & (multiDF["cancelled"] == False)]) / len(multiDF.loc[multiDF["cancelled"] == False])) * 100))
+print("Overall success rate: %d%%" % ((len(multiDF.loc[multiDF["numSV"] >= 5]) / len(multiDF)) * 100))
