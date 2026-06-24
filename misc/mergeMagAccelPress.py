a3d9f694d1969e26867e0d4003f05ec9f558aaa2
Will Stuchbury <w.stuchbury@pathtrack.co.uk>
2026-06-24 14:58:12 +0100

    Added script for merging accel, mag and pressure data (for wildbyte)

---
+++ misc/mergeMagAccelPress.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+ROOT = "./"
+BASE_NAME = "Obs240626_120700_Tag10597"
+
+accelDF = pd.read_pickle(ROOT+BASE_NAME+"_Accel.pkl")
+magDF = pd.read_pickle(ROOT+BASE_NAME+"_Magnetometer.pkl")
+pressDF = pd.read_pickle(ROOT+BASE_NAME+"_Differential_Pressure.pkl")
+
+accelDF.rename(columns={"X":"accX", "Y":"accY", "Z":"accZ"}, inplace=True)
+
+accelDF.drop(columns=["line", "validation", "mag"], inplace=True)
+
+def assignMagToAccel(magRow):
+    global accelDF
+    timeDiff = abs(accelDF["datetime"] - magRow["datetime"])
+    accelDF.loc[timeDiff.idxmin(), ["magX","magY","magZ"]] = (magRow["X"], magRow["Y"], magRow["Z"])
+
+def assignPressToAccel(pressRow):
+    global accelDF
+    timeDiff = abs(accelDF["datetime"] - pressRow["datetime"])
+    accelDF.loc[timeDiff.idxmin(), ["pressure"]] = pressRow["pressure"]
+
+magDF.apply(lambda row: assignMagToAccel(row),axis=1)
+
+pressDF.apply(lambda row: assignPressToAccel(row),axis=1)
+
+accelDF.to_csv(BASE_NAME+"_merged.csv",index=False)
