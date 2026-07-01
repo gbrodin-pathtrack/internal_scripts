a57986efc861b70c58cc583c3cab2b906456e723
Will Stuchbury <w.stuchbury@pathtrack.co.uk>
2026-07-01 12:00:54 +0100

    Added CLI option to mergeMagAccelPress (WildByte)

--- misc/mergeMagAccelPress.py
+++ misc/mergeMagAccelPress.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import sys
 
 ROOT = "./"
 BASE_NAME = "Obs240626_120700_Tag10597"
+
+if len(sys.argv) > 1:
+    BASE_NAME = sys.argv[1]
 
 accelDF = pd.read_pickle(ROOT+BASE_NAME+"_Accel.pkl")
 magDF = pd.read_pickle(ROOT+BASE_NAME+"_Magnetometer.pkl")
