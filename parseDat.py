0198b58764582e746568ff113f0d5a02b573b54c
Will Stuchbury <w.stuchbury@pathtrack.co.uk>
2026-08-21 11:42:46 +0100

    Revert "Test commit for repo move"
    This reverts commit 0d1a0047e63cb48235096e91ab3c8e0108c1bd65.

--- parseDat.py
+++ parseDat.py
@@ -24,8 +24,6 @@ from parsers.parseImmersion import parseImmersionLine
 from parsers.parseMagnetometer import parseMagnetometerLine
 from parsers.parseMagAccel import parseMagAccelLine
 
-#test comment for repo move
-
 USE_PICKLE = False
 
 UNSCRAMBLE = False
