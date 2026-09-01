0e242a75616afe47387ead8189cd92e47e573196
Will Stuchbury <w.stuchbury@pathtrack.co.uk>
2026-09-01 16:24:26 +0100

    Added support for converting base station ".dat" files to ".ptdb" (with 0xFD contact line insertion)

--- datToPtd.py
+++ datToPtd.py
@@ -4,8 +4,15 @@ import sys
 
 ROOT = "./"
 
-def convertDatFile(fileName : str):
-    with open(fileName, 'r') as dat, open(splitext(fileName)[0]+".ptdl", 'wb') as ptd:
+def convertDatFile(fileName : str, uhf : bool):
+    if uhf:
+        extension = ".ptdb"
+    else:
+        extension = ".ptdl"
+
+    tagID = -1
+
+    with open(fileName, 'r') as dat, open(splitext(fileName)[0]+extension, 'wb') as ptd:
         headerBytes = bytes([0x44, 0x58, 0x49, 0x46, #FIXD
                              0x01, #Version 1
                              0x00, 0x02, #512 byte lines
@@ -17,10 +24,31 @@ def convertDatFile(fileName : str):
         while (line := dat.readline()) != "":
             if not line[:1].isdigit():
                 continue
-            byteLine = bytes(int(byte) for byte in line.split()) #will be 512 bytes
-            if len(byteLine) != 512:
+            intLine = [int(byte) for byte in line.split()]
+
+            if uhf and len(intLine) != 516:
+                print("line not 516 bytes long")
+                exit(1)
+            elif not uhf and len(intLine) != 512:
                 print("line not 512 bytes long")
                 exit(1)
+
+            if uhf:
+                intLine = intLine[:512]
+                lineTagID = intLine[3] + (intLine[4] << 8)
+                if lineTagID != tagID:
+                    contactInts = [0 for _ in range(512)]
+                    contactInts[0] = 0xFD
+                    contactInts[1] = 18
+                    contactInts[3] = lineTagID & 0xFF
+                    contactInts[4] = (lineTagID >> 8) & 0xFF
+                    contactInts[5] = (lineTagID >> 16) & 0xFF
+                    contactInts[6] = (lineTagID >> 24) & 0xFF
+                    contactBytes = bytes(contactInts)
+                    ptd.write(contactBytes)
+                    tagID = lineTagID
+
+            byteLine = bytes(intLine)
             ptd.write(byteLine)
 
 
@@ -36,4 +64,4 @@ else:
 
 for fileName in wantedFiles:
     #only produce a combined file if there are more than 1 dat files
-    convertDatFile(fileName)
+    convertDatFile(fileName, fileName.find("BS") != -1)
